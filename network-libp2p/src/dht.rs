91dc023fa9cda4958dfb27d2c3d5eba67ed44bcf
Sebastian <sebastian@nimiq.com>
2024-11-12 13:55:29 -0600

    Add some sanity checking for Dht records.

--- network-libp2p/src/dht.rs
+++ network-libp2p/src/dht.rs
@@ -1,17 +1,25 @@
 use libp2p::{kad::Record, PeerId};
 use nimiq_keys::Address;
+use nimiq_network_interface::network::Network as NetworkInterface;
 use nimiq_serde::DeserializeError;
 use nimiq_validator_network::validator_record::ValidatorRecord;
 
 pub use crate::network_types::DhtRecord;
+use crate::Network;
 
 #[derive(Debug)]
 pub enum DhtVerifierError {
     MalformedTag,
+    UnknownTag,
     MalformedKey(DeserializeError),
     MalformedValue(DeserializeError),
-    UnknownTag,
     UnknownValidator(Address),
+    AddressMismatch(Address, Address),
+    PublisherMissing,
+    PublisherMismatch(
+        <Network as NetworkInterface>::PeerId,
+        <Network as NetworkInterface>::PeerId,
+    ),
     StateIncomplete,
     InvalidSignature,
 }
