6f69bd794ddb8df36dc5eb25cd8d6e50748e9b25
Sören <hello@soerenschwert.de>
2024-11-13 18:11:36 -0600

    Set new network config settings for the web-client
    Otherwise these settings default to 0.

--- web-client/src/client/lib.rs
+++ web-client/src/client/lib.rs
@@ -162,6 +162,9 @@ impl Client {
         config.network.only_secure_ws_connections = true;
         config.network_id = web_config.network_id;
         config.network.desired_peer_count = 6;
+        config.network.peer_count_max = 50;
+        config.network.peer_count_per_ip_max = 10;
+        config.network.peer_count_per_subnet_max = 10;
 
         log::info!(?config, "Final configuration");
 
