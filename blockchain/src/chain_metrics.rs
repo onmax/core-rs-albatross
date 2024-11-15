15b18ff322a93e545f5c53991eddefb1c1b6e799
styppo <me@styppo.com>
2024-11-15 20:50:16 +0000

    Metrics: add equivocation proof counts

--- blockchain/src/chain_metrics.rs
+++ blockchain/src/chain_metrics.rs
@@ -1,4 +1,4 @@
-use nimiq_block::Block;
+use nimiq_block::{Block, EquivocationProof};
 use nimiq_blockchain_interface::{ChunksPushError, ChunksPushResult, PushError, PushResult};
 use nimiq_hash::Blake2bHash;
 use prometheus_client::{
@@ -12,6 +12,7 @@ pub struct BlockchainMetrics {
     block_push_counts: Family<PushResultLabels, Counter>,
     transactions_counts: Family<TransactionProcessedLabels, Counter>,
     skip_blocks: Counter,
+    equivocation_counts: Family<EquivocationProofLabels, Counter>,
 }
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
@@ -41,6 +42,18 @@ enum TransactionProcessed {
     Reverted,
 }
 
+#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
+struct EquivocationProofLabels {
+    ty: EquivocationProofType,
+}
+
+#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelValue)]
+enum EquivocationProofType {
+    Fork,
+    DoubleVote,
+    DoubleProposal,
+}
+
 impl BlockchainMetrics {
     pub fn register(&self, registry: &mut Registry) {
         registry.register(
@@ -59,6 +72,12 @@ impl BlockchainMetrics {
             "skip_blocks",
             "Number of skip blocks applied",
             self.skip_blocks.clone(),
+        );
+
+        registry.register(
+            "equivocation_counts",
+            "Count of equivocation proofs applied",
+            self.equivocation_counts.clone(),
         );
     }
 
@@ -104,6 +123,20 @@ impl BlockchainMetrics {
         if block.is_skip() {
             self.skip_blocks.inc();
         }
+
+        if block.is_micro() && block.has_body() {
+            let body = block.unwrap_micro_ref().body.as_ref().unwrap();
+            for equivocation in &body.equivocation_proofs {
+                let ty = match equivocation {
+                    EquivocationProof::Fork(_) => EquivocationProofType::Fork,
+                    EquivocationProof::DoubleVote(_) => EquivocationProofType::DoubleVote,
+                    EquivocationProof::DoubleProposal(_) => EquivocationProofType::DoubleProposal,
+                };
+                self.equivocation_counts
+                    .get_or_create(&EquivocationProofLabels { ty })
+                    .inc();
+            }
+        }
     }
 
     #[inline]
@@ -123,17 +156,7 @@ impl BlockchainMetrics {
         }
 
         for (_, block) in adopted_blocks {
-            if block.is_micro() {
-                self.transactions_counts
-                    .get_or_create(&TransactionProcessedLabels {
-                        ty: TransactionProcessed::Applied,
-                    })
-                    .inc_by(block.num_transactions() as u64);
-            }
-
-            if block.is_skip() {
-                self.skip_blocks.inc();
-            }
+            self.note_extend(block);
         }
     }
 }
